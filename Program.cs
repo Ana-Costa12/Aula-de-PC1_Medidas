3a3bbd7ef239e5289ead7cab6d06a3312819f80b
Ana-Costa12 <claudiasantborges@gmail.com>
2021-03-20 20:48:39 -0300

    TRI Conversão

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Aula_de_PC1_Medidas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double metros;
+            double km = 0.001;
+            double cm = 100;
+
+            Console.WriteLine("Vamos transformar metros em centímetros e quilômetros!\n------------------------------------------------------\n\nTecle enter para começar:");
+            Console.ReadLine();
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.Write("Digite o valor de metros para a conversão:");
+            Console.ResetColor();
+            metros = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Tecle enter para ver os resultados:");
+            Console.ReadLine();
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.Write($"{metros} metros é equivalente a:\n{metros * cm} Centímetros\nE\n{metros * km} Quilômetros");
+            Console.ResetColor();
+
+        }
+    }
+}
